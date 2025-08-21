d396fa960894faa1523245816da29dc4c9c77ec5
Leon Hsieh <leonhsieh@Leons-MacBook-Pro-2.local>
2025-08-20 19:55:35 -0700

    added sentry for crash analytics

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'bobadex_app.dart';
@@ -31,8 +32,14 @@ void main() async {
     );
 
     await Hive.initFlutter();
-    
-    runApp(const BobadexApp());
+
+    await SentryFlutter.init(
+      (o) {
+        o.dsn = dotenv.env['SENTRY_DSN'];
+        o.tracesSampleRate = 0.1;
+      },
+      appRunner: () => runApp(const BobadexApp()),
+    );
   } catch (e) {
     debugPrint('Failed to initialize app: $e');
     runApp(MaterialApp(
